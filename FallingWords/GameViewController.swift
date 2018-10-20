8e86612917a6515977d0943bb1217c43a44d59e0
Nataly <ngranetchi@gmail.com>
2018-10-20 10:37:53 +0200

    Add game screen and segue to it from welcome screen

---
+++ FallingWords/GameViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GameViewController.swift
+//  FallingWords
+//
+//  Created by Nataly Tatarintseva on 10/20/18.
+//  Copyright © 2018 Nataly Tatarintseva. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var labelOne: UILabel!
+    @IBOutlet weak var labelTwo: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
